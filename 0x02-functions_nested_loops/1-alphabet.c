4d7d2b035c708a76d7507a6d38440c9b56d8fe84
Eboobz <echianuebube@yahoo.com>
2023-02-25 07:17:19 -0800

    hey

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - prints alphabet, followed by a new line
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+print_alphabet(a,b,c...z);
+return (0);
+}
