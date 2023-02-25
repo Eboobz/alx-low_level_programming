5b369dcf6374f5e57b2cac6f1372f7fe9829ec48
Eboobz <echianuebube@yahoo.com>
2023-02-25 05:25:40 -0800

    add

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,8 +2,9 @@
 
 /**
  * main - prints _putchar, followed by a new line
- * Return: Always 0 (Success)  
-int main(void)
+ * Return: Always 0 (Success)
+*/
+ int main(void)
 {
 	_putchar("_putchar\n");
        return (0);
