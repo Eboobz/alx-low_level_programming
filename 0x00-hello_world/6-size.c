2b8e789563ffd8de56c88a8f24fec1c28672b64b
Eboobz <echianuebube@yahoo.com>
2023-02-26 01:42:31 -0800

    Hey

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ long int l;
 long long int m;
 float f;
 
-printf("Size of a char: %lu\n", (unsigned long)sizeof(c) byte(s));
-printf("Size of an int: %lu\n", (unsigned long)sizeof(i) byte(s));
-printf("Size of a long int: %lu\n", (unsigned long)sizeof(l) byte(s));
-printf("Size of a long long int: %lu\n", (unsigned long)sizeof(m) byte(s));
-printf("Size of a float: %lu\n", (unsigned long)sizeof(f) byte(s));
+printf("Size of a char: %lu\n", "byte(s)" (unsigned long)sizeof(c));
+printf("Size of an int: %lu\n", "byte(s)" (unsigned long)sizeof(i));
+printf("Size of a long int: %lu\n", "byte(s)" (unsigned long)sizeof(l));
+printf("Size of a long long int: %lu\n", "byte(s)" (unsigned long)sizeof(m));
+printf("Size of a float: %lu\n", "byte(s)" (unsigned long)sizeof(f));
 
 return (0);
 }
