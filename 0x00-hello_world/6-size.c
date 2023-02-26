2f776b9e430d8dfeb5680a67b3e5d3e7a1d41e06
Eboobz <echianuebube@yahoo.com>
2023-02-26 01:08:10 -0800

    Thank you

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - Etry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char c;
+int i;
+long int l;
+long long int m;
+float f;
+
+printf("Size of a char\: %lu\n, (unsigned long)sizof(c));
+printf("Size of an int\: %lu\n, (unsigned long)sizof(i));
+printf("Size of a long int\: %lu\n, (unsigned long)sizof(l));
+printf("Size of a long long int\: %lu\n, (unsigned long)sizof(m));
+printf("Size of a float\: %lu\n, (unsigned long)sizof(f));
+
+return (0);
+}
