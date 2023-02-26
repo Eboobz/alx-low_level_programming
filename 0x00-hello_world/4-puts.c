39f44090fc5b9cdb37fd6582b5fcbcdfe140b384
Eboobz <echianuebube@yahoo.com>
2023-02-25 22:47:59 -0800

    yea

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdioo.h>
+
+/**
+*main - print statement, followed by a new line
+*Return: Always 0 (Success)
+*/  
+int puts (void)
+{
+_putchar("Programming is like buiding a multilingual puzzle\n");
+
+return (0)
+}
