07aa92106db5edc1ec0ee9b2d70bb428e87f4733
Eboobz <echianuebube@yahoo.com>
2023-03-02 22:04:23 -0800

    add

---
+++ 0x01-variables_if_else_while/2-print_lphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - prints alphabet followed by a new line
+*
+*Return: Always 0
+*/
+int main(void)
+{
+char c = a
+
+  while (c > a)
+    _putchar alphabet("c\n");
+
+ return (0);
+   }
