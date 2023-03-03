169dfd3e6943eacb62aa9d7c0ccc2406e3afc88b
Eboobz <echianuebube@yahoo.com>
2023-03-02 22:37:24 -0800

    add

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - prints alphabet in lower case
+*followed by a new line
+*
+*Return: Always 0
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c = 'z': c++) 
+{ 
+putchar ("c\n");
+}
+return (0);
+}
