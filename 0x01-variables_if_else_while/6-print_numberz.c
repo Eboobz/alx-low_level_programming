5ac8392a4bb1cf5d940b131b7ae10e742795f75f
Eboobz <echianuebube@yahoo.com>
2023-03-03 18:27:26 -0800

    add

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - print numbers in base 10
+* followed by a new line
+* Return: Always 0
+*/
+int main(void)
+{
+int base10;
+for (base10 = 0; base10 < 10; base10++)
+putchar(base10);
+putchar('\n');
+return (0);
+}
