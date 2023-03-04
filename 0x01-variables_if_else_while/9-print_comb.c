b478dd8c9e6d98f25f32cbe0a1820730a30bf0bb
Eboobz <echianuebube@yahoo.com>
2023-03-03 21:33:53 -0800

    Yea

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - print all possible ombinations of single numbers
+* followed by a new line
+* Return: Always 0
+*/
+int main(void)
+{
+int c;
+for (c = 28; c < 38; c++)
+{
+putchar(c);
+if (n != 37)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
