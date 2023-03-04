0368d12c15af1dd38da692d0bfd68f5df8958d44
Eboobz <echianuebube@yahoo.com>
2023-03-03 18:13:58 -0800

    add

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print numbers in base 10
+* followed by a new line
+* Return: Always 0
+*/
+int main(void)
+{
+int base10;
+for (base10 = 0; base10 < 10; base10++)
+printf("%d\n", base10);
+return (0);
+}
