4298b8b73ee407c78bf90b6456172d070802914a
Eboobz <echianuebube@yahoo.com>
2023-03-07 04:54:27 -0800

    Yea

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 /**
-* main - prints alphabet, followed by a new line
-* Return: Always 0 (Success)
+* main - prints alphabet
+* followed by a new line
 */
 void print_alphabet(void)
 {
 char ch;
 ch = 'a';
-while (ch <= 'z') 
-print_alphabet(ch\n);
-return (0);
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+} 
+_putchar('\n');
 }
