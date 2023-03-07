9b958ced573eb2c7def4335ed6ba4fceb9493dfd
Eboobz <echianuebube@yahoo.com>
2023-03-06 19:35:58 -0800

    Ya

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strspn - Returns the number of bytes in the initial segment.
+* 0f s which consist only of bytes from accept
+* @s: A pointer to the s array
+* @accept: The pointer to the accept array
+*
+* Return: A pointer to the filled memory area @dest.
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != s[i]; j++)
+{
+if (accept [j] != '\0')
+{
+return (i);
+}
+}
+}
+return (i);
+}
