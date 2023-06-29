bf44034a756691e8ca5b57d407e397b147ba0d4e
Eboobz <echianuebube@yahoo.com>
2023-06-28 17:05:55 -0700

    good

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints 10 times the alphabet, in lowercase, 
+* followed by a new line
+*/
+char ch;
+Int i;
+i = 0
+while (i < 10)
+{
+ch = 'a';
+hile (ch <= "z")
+{
+_putchar(ch);
+ch++;
+}
+_putchaar('\n');
+i++;
+}
+} 
